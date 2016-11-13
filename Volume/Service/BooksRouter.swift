519021e21152abe726217d4e24f69d53ec705815
Vlad Radu <vladradu@Vlads-MBP.home>
2016-11-13 17:22:58 +0000

    updated router

---
+++ Volume/Service/BooksRouter.swift
@@ -0,0 +1,15 @@
+//
+//  BooksRouter.swift
+//  Volume-iOS
+//
+//  Created by Vlad Radu on 12/11/2016.
+//  Copyright © 2016 Vlad Radu. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+public enum BooksRouter: URLRequestConvertible {
+  static let baseURLPath = "" // update to our api base url
+  static let apiVersionNumber = 1
+}
